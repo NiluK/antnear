09f3e17ba819192850e9e317ccf6a54b1ea88fb2
Rui Duan - work <ruiduan1@Ruis-MBP.optusnet.com.au>
2022-01-23 10:00:10 +1100

    add airtable api

---
+++ pages/api/airtable-create.ts
@@ -0,0 +1,48 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  id: string;
+  error: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  var Airtable = require("airtable");
+  var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+    "appZJm9vl67EuISuq"
+  );
+  const ids: any = [];
+  if (req.method !== "POST") {
+    res.status(400).send({ id: "", error: "Only POST requests allowed" });
+    return;
+  }
+  try {
+    base("Table 1").create(
+      [
+        {
+          fields: {
+            Name: "Task 3",
+            Notes: "third note",
+            Status: "Todo",
+          },
+        },
+      ],
+      function (err: any, records: any) {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ id: "", error: "failed to create data" });
+        }
+        records.forEach(function (record: any) {
+          console.log(record.getId());
+          ids.push(record.getId());
+        });
+        res.status(200).json({ id: ids, error: "" });
+      }
+    );
+  } catch (err) {
+    res.status(500).json({ id: "", error: "failed to create data" });
+  }
+}
---
+++ pages/api/airtable-list.ts
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+  error: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  var Airtable = require("airtable");
+  var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+    "appZJm9vl67EuISuq"
+  );
+  const names: any = [];
+  try {
+    const results = await base("Table 1")
+      .select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 30,
+        view: "Grid view",
+      })
+      .eachPage(function page(records: any, fetchNextPage: any) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach(function (record: any) {
+          names.push(record.get("Name"));
+        });
+        res.status(200).json({ name: names, error: "" });
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+      });
+  } catch (err) {
+    res.status(500).json({ name: "", error: "failed to load data" });
+  }
+}
